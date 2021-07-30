9deb7aca0aeddce23bf4691507d9fda1b3af0e20
Ago Balan <jehovabalan@gmail.com>
2021-07-29 20:45:11 -0400

    Update WebdriverSettings.java

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebdriverSettings {
 	
 	public WebDriver driverSettings() {
+		 String currentDirectory = System.getProperty("user.dir");
+      System.out.println("The current working directory is " + currentDirectory);
 		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver_win32/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		return driver;
